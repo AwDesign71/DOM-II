118d3337559a46cdb248a2d2150c4fc42929a12e
bigdre4557@gmail.com <amwilson2019@gmail.com>
2019-06-18 16:33:34 -0400

    Created the mouseover working keydown

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+//mouseover event
+const over = document.querySelector('header nav');
+let backgroundColor;
+over.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'blue';
+    //resting the color back
+    setTimeout(() => {
+     event.target.style.color = '';
+    }, 500);
+    });
+    //Keydown event
+    const downEvent = document.querySelector(' .intro p');
+    downEvent.addEventListener('keydown', keyEvent, false);
+         function keyEvent() {
+            if(event.keyCode == '13') {
+                event.target.style.backgroundColor = 'yellow';
+                setTimeout(() => {
+                    event.target.style.backgroundColor = '';
+                   }, 5000);
+               }
+         }
